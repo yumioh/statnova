47f5690ee576100f60f342d30bf4d39f80384734
yumi oh <iove0103@naver.com>
2025-01-08 01:22:34 +0900

    시각화수정

--- chat_anaylsis/3_data_visualization.py
+++ chat_anaylsis/3_data_visualization.py
@@ -36,22 +36,22 @@ def filter_date(df, start_date, end_date):
     return df[condition_date]
 
 #LDA 모델링 및 파일 저장
-def lda_modeling_and_visualization(corpus, dictionary,start_date):
-    num_topics = 7  # 원하는 주제 수
-    lda_model = LdaMulticore(corpus, num_topics=num_topics, id2word=dictionary, passes=10, workers=6, random_state=42)
+# def lda_modeling_and_visualization(corpus, dictionary,start_date):
+#     num_topics = 7  # 원하는 주제 수
+#     lda_model = LdaMulticore(corpus, num_topics=num_topics, id2word=dictionary, passes=10, workers=6, random_state=42)
 
-    # 각 주제별로 중요한 단어 확인
-    for idx, topic in enumerate(lda_model.print_topics(num_words=5)):
-        print(f"Topic {idx}: {topic}")
+#     # 각 주제별로 중요한 단어 확인
+#     for idx, topic in enumerate(lda_model.print_topics(num_words=5)):
+#         print(f"Topic {idx}: {topic}")
 
-    # 문서별 주제 분포 확인
-    for index, topic_dist in enumerate(lda_model[corpus][:5]):
-        print(f"Document {index}: {topic_dist}")
+#     # 문서별 주제 분포 확인
+#     for index, topic_dist in enumerate(lda_model[corpus][:5]):
+#         print(f"Document {index}: {topic_dist}")
 
-    # pyLDAvis로 시각화 준비 및 저장
-    filename = f'./chat_anaylsis/img/lda_{start_date}.html'
-    vis = gensimvis.prepare(lda_model, corpus, dictionary)
-    pyLDAvis.save_html(vis, filename)
+#     # pyLDAvis로 시각화 준비 및 저장
+#     filename = f'./chat_anaylsis/img/lda_{start_date}.html'
+#     vis = gensimvis.prepare(lda_model, corpus, dictionary)
+#     pyLDAvis.save_html(vis, filename)
 
 print("---------------------워드 클라우드------------------------")
 
@@ -81,7 +81,7 @@ DataVisualizer.create_wordcloud(filtered_data, font_path, filename)
 
 print("---------------------키워드별 워드 클라우드------------------------")
 
-exclude_keywords = ["강의","과목","수업","시험","문제","과제","출석","혹시","학기","교수"]
+exclude_keywords = ["강의","과목","수업","시험","문제","과제","출석","혹시","학기","교수","자료","캠퍼스","교재","공부","지역"]
 keyword = "교수"
 
 # 한글 폰트 설정
@@ -108,7 +108,7 @@ DataVisualizer.create_wordcloud(filtered_data, font_path, filename)
 print("-------------------최빈어별 BAR 그래프------------------------")
 
 keywords = ["과목", "학기", "과제", "수업", "시험", "문제", "출석", "혹시", "통계", "강의", "교수"]
-counts = [9656, 6798, 5959, 5316, 5475, 4531, 3965, 3930, 5910, 3652, 3540]
+counts = [9656, 6798, 5959, 5316, 5475, 4531, 3965, 3930, 3300, 3652, 3540]
 
 # 막대 그래프 생성
 plt.figure(figsize=(12, 6))
@@ -129,7 +129,7 @@ plt.tight_layout()
 plt.show()
 
 print("---------------------LDA 학습------------------------")
-# #multiprocessing을 사용하는 코드로 main을 넣어줘어야함
+#multiprocessing을 사용하는 코드로 main을 넣어줘어야함
 # if __name__ == '__main__':
 #     #딕셔너리 생성 
 #     dictionary = Dictionary(filtered_data['content'])
@@ -206,7 +206,7 @@ plt.ylabel('Join Count', fontsize=12)
 plt.tight_layout()
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.savefig("./chat_anaylsis/img/line_graph_join_dayname.png", dpi=300)
-plt.show()
+##plt.show()
 
 print("---------------------요일별 나가는 채팅 선그래프------------------------")
 
@@ -231,7 +231,7 @@ plt.ylabel('Left Count', fontsize=12)
 plt.tight_layout()
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.savefig("./chat_anaylsis/img/line_graph_left_dayname.png", dpi=300)
-plt.show()
+#plt.show()
 
 print("---------------------시간별 입장하는 채팅 선그래프------------------------")
 join_df = pd.read_csv("./chat_anaylsis/data/join_messages.csv")
@@ -263,31 +263,31 @@ plt.tight_layout()
 #plt.show()
 
 print("---------------------시간별 나가는 채팅 선그래프------------------------")
-left_df = pd.read_csv("./chat_anaylsis/data/left_messages.csv")
+# left_df = pd.read_csv("./chat_anaylsis/data/left_messages.csv")
 
-# 시간대별 데이터 집계
-left_df['datetime'] = pd.to_datetime(left_df['date'] + ' ' + left_df['time'], format="%y-%m-%d %H:%M")
-left_df['hour'] = left_df['datetime'].dt.hour
+# # 시간대별 데이터 집계
+# left_df['datetime'] = pd.to_datetime(left_df['date'] + ' ' + left_df['time'], format="%y-%m-%d %H:%M")
+# left_df['hour'] = left_df['datetime'].dt.hour
 
-# 시간대별 데이터 집계
-left_by_hour = left_df['hour'].value_counts().sort_index().reset_index()
-left_by_hour.columns = ["hour", "count"]
+# # 시간대별 데이터 집계
+# left_by_hour = left_df['hour'].value_counts().sort_index().reset_index()
+# left_by_hour.columns = ["hour", "count"]
 
-plt.figure(figsize=(10, 6))
-plt.plot(left_by_hour['hour'], left_by_hour['count'], marker='o', color='#00509E', label='Join Count', linewidth=2)
+# plt.figure(figsize=(10, 6))
+# plt.plot(left_by_hour['hour'], left_by_hour['count'], marker='o', color='#00509E', label='Join Count', linewidth=2)
 
-# 값 표시
-for x, y in zip(left_by_hour['hour'], left_by_hour['count']):
-    plt.text(x, y, str(y), fontsize=10, ha='center', va='bottom', color='black')
+# # 값 표시
+# for x, y in zip(left_by_hour['hour'], left_by_hour['count']):
+#     plt.text(x, y, str(y), fontsize=10, ha='center', va='bottom', color='black')
 
-# 그래프 설정
-plt.title('Lefts by Hour', fontsize=14)
-plt.xlabel('Hour', fontsize=12)
-plt.ylabel('Left Count', fontsize=12)
-plt.xticks(range(24), labels=[f'{h}:00' for h in range(24)], rotation=45)
-plt.grid(True, linestyle='--', alpha=0.7)
-plt.savefig("./chat_anaylsis/img/line_graph_left_hour.png", dpi=300)
-plt.tight_layout()
+# # 그래프 설정
+# plt.title('Lefts by Hour', fontsize=14)
+# plt.xlabel('Hour', fontsize=12)
+# plt.ylabel('Left Count', fontsize=12)
+# plt.xticks(range(24), labels=[f'{h}:00' for h in range(24)], rotation=45)
+# plt.grid(True, linestyle='--', alpha=0.7)
+# plt.savefig("./chat_anaylsis/img/line_graph_left_hour.png", dpi=300)
+# plt.tight_layout()
 
 #plt.show()
 
